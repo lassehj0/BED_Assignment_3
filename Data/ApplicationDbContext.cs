0d757d6a6015193dd887d86237fb98a72a4b93b2
lassehj0 <lassehj00@github.com>
2022-11-16 11:58:27 +0100

    Chaaaanges

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Assignment3.Models;
 
 namespace Assignment3.Data
 {
@@ -10,7 +10,8 @@ namespace Assignment3.Data
             : base(options)
         {
         }
-        public DbSet<Assignment3.Models.CheckIns> CheckIns { get; set; }
-        public DbSet<Assignment3.Models.TotalBookingsPerDay> TotalBookingsPerDay { get; set; }
+
+        public DbSet<CheckIns> CheckIns { get; set; }
+        public DbSet<TotalBookingsPerDay> TotalBookingsPerDay { get; set; }
     }
 }
---
+++ Data/Migrations/20221116105601_NewTables.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignment3.Data.Migrations
+{
+    public partial class NewTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckIns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomNumber = table.Column<int>(type: "int", nullable: false),
+                    Adults = table.Column<int>(type: "int", nullable: false),
+                    Children = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckIns", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TotalBookingsPerDay",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalAdults = table.Column<int>(type: "int", nullable: false),
+                    TotalChildren = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TotalBookingsPerDay", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckIns");
+
+            migrationBuilder.DropTable(
+                name: "TotalBookingsPerDay");
+        }
+    }
+}
