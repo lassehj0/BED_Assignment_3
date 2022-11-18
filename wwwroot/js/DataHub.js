1ccdf93de1d354c2f11528696a757367f677770f
oscarboe <Oscarboen@gmail.com>
2022-11-18 13:13:21 +0100

    yolo

---
+++ wwwroot/js/DataHub.js
@@ -0,0 +1,11 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/DataHub").build();
+
+connection.on("update", function () {
+
+    document.window.location.reload();
+});
+
+connection.start().then(function () {
+}).catch(function (err) {
+    console.error(err.toString());
+});
